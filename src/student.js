c43a6ae10b66d97995e1e6f2ff219723555a34cd
joshlynj <joshlyn294@gmail.com>
2022-01-21 14:35:29 -0700

    final

--- src/student.js
+++ src/student.js
@@ -1,28 +1,33 @@
 class Student {
-    constructor(...param){
-        this.name=param[0];
-        this.skillLevel=param[1];
+    constructor(name, skillLevel){
+        this.name=name;
+        this.skillLevel=skillLevel;
         this.assignments=[];
     }
+
     study(){
       if(this.skillLevel===100){
           return this;
-      }
+      } else{
       this.skillLevel+=1;
       return this;
+      } 
     }
-    doHomework(...objParam){
 
-      if(!objParam.length){
-          console.log("dkdkdk ",objParam);
+    doHomework(homework){
+    if (homework === undefined){
+      for (let i=0;i<this.assignments.length;i++){
+        if (this.assignments[i].completed === false && this.assignments[i].skillLevel <= this.skillLevel){
+          this.assignments[i].completed = true;
+        }
       }
-      // if(objParam.skillLevel<this.skillLevel && objParam.skillLevel>1){
-      //     this.assignments.push(objParam);
-      //     objParam.completed=true;
-      // }else{
-      //     objParam.completed=false;
-      // }
+    } else if (homework.skillLevel >= 1 && homework.skillLevel <=100 && homework.skillLevel <= this.skillLevel){
+      homework.completed = true;
+      this.assignments.push(homework);
+    } else if (homework.skillLevel >= 1 && homework.skillLevel <=100 && homework.skillLevel > this.skillLevel) {
+      homework.completed = false;
+      this.assignments.push(homework);
     }
   }
-
+} 
   module.exports = Student
